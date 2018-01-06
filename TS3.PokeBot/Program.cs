48bcf9d7a52cc309881dcc24cd3011128c14bc7c
krzysztof <krzysztof@grywamtu.pl>
2018-01-06 19:32:41 +0100

    Dodaj pliki projektów.

---
+++ TS3.PokeBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TS3.PokeBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
