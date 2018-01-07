ce9f3ba43e5f44f6ea272042ee0ebbdfd5fe17c0
krzysztof <krzysztof@grywamtu.pl>
2018-01-07 16:01:55 +0100

    Added ConfigurationBuilder

---
+++ TS3.PokeBot/Config/AutoPoke.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TS3.PokeBot.Config
+{
+    public class AutoPoke
+    {
+        public AutoPoke()
+        {
+            //if (Initialization)
+                Channels = new Dictionary<ushort, AutoPokeConfig>() { { 0, new AutoPokeConfig() } };
+        }
+        public bool Enabled { get; set; } = true;
+        public Dictionary<ushort, AutoPokeConfig> Channels = new Dictionary<ushort, AutoPokeConfig>();
+    }
+    public class AutoPokeConfig
+    {
+        public AutoPokeConfig()
+        {
+            //if (Initialization)
+                Groups = new List<AutoPokeGroup>() { new AutoPokeGroup() };
+        }
+        public List<AutoPokeGroup> Groups = new List<AutoPokeGroup>();
+    }
+    public class AutoPokeGroup
+    {
+        public ushort Id { get; set; }
+        public int DelayRelative { get; set; } = 0;
+        public int DelayAbsolute { get; set; } = 0;
+    }
+}
---
+++ TS3.PokeBot/Config/Ts3BotConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace TS3.PokeBot.Config
+{
+    public class Ts3BotConfig
+    {
+        public Ts3BotConfig()
+        {
+            Server = new Server();
+            Bot = new Bot();
+        }
+        public Server Server { get; set; }
+        public Bot Bot { get; set; }
+    }
+}
